76ddf43e06f2421206f95b3c4aa7d99260cde122
BubbleSothoth <105468190+BubbleSothoth@users.noreply.github.com>
2025-02-27 19:44:55 +0800

    Add files via upload

--- Tools/HashAlgorithm.cpp
+++ Tools/HashAlgorithm.cpp
@@ -74,3 +74,67 @@ bool Tools::HashAlgorithm::MD5(const char* filePath, LPBYTE hashBuffer) {
 bool Tools::HashAlgorithm::MD5(const std::string filePath, LPBYTE hashBuffer) {
     return Tools::HashAlgorithm::MD5(filePath.c_str(), hashBuffer);
 }
+
+bool Tools::HashAlgorithm::MD5(const BYTE* byteString, size_t byteSize, LPBYTE hashBuffer)
+{
+
+    // 创建哈希算法句柄
+    HCRYPTPROV hProv;
+    HCRYPTHASH hHash;
+    if (!CryptAcquireContext(&hProv, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT) || !CryptCreateHash(hProv, CALG_MD5, 0, 0, &hHash)) {
+        std::cerr << "Failed to create hash." << std::endl;
+        return false;
+    }
+
+    // 计算哈希值
+    LPBYTE buffer;
+    buffer = (LPBYTE)malloc(byteSize * sizeof(BYTE));
+    if (buffer == nullptr) {
+        std::cerr << "Memory error." << std::endl;
+        return false;
+    }
+    memset(buffer, 0, byteSize * sizeof(BYTE));
+    memcpy_s(buffer, byteSize * sizeof(BYTE), byteString, byteSize * sizeof(BYTE));
+    if (!CryptHashData(hHash, buffer, byteSize, 0)) {
+        std::cerr << "Failed to hash data." << std::endl;
+        CryptDestroyHash(hHash);
+        CryptReleaseContext(hProv, 0);
+        return false;
+    }
+    free(buffer);
+
+    // 获取哈希值
+    DWORD hashSize = 16;
+    if (!CryptGetHashParam(hHash, HP_HASHVAL, hashBuffer, &hashSize, 0)) {
+        std::cerr << "Failed to get hash value." << std::endl;
+        CryptDestroyHash(hHash);
+        CryptReleaseContext(hProv, 0);
+        return false;
+    }
+
+    // 清理资源
+    CryptDestroyHash(hHash);
+    CryptReleaseContext(hProv, 0);
+
+    return true;
+}
+
+bool DLLAPI Tools::HashAlgorithm::Test(void)
+{
+    printf_s("Test.dll\n");
+    return false;
+}
+
+void Tools::HashAlgorithm::SHA256(void* buffer, std::string path)
+{
+}
+
+bool Tools::HashAlgorithm::MD5Equal(const BYTE md5_1[MD5Size], const BYTE md5_2[MD5Size])
+{
+    return !memcmp(md5_1, md5_2, MD5Size);
+}
+
+bool Tools::HashAlgorithm::SHA256Equal(const BYTE sha256_1[SHA256Size], const BYTE sha256_2[SHA256Size])
+{
+    return !memcmp(sha256_1, sha256_2, SHA256Size);
+}
--- Tools/HashAlgorithm.h
+++ Tools/HashAlgorithm.h
@@ -2,6 +2,19 @@
 
 #include "../DLLAPI.h"
 
+#ifndef MD5Size
+
+#define MD5Size (unsigned)128/8
+
+#endif // !MD5Size
+
+#ifndef SHA256Size
+
+#define SHA256Size (unsigned)256/8
+
+#endif // !MD5Size
+
+
 #include <string>
 #include <Windows.h>
 namespace Tools
@@ -9,7 +22,10 @@ namespace Tools
 	namespace HashAlgorithm {
 		bool DLLAPI MD5(const char* filePath, LPBYTE hashBuffer);
 		bool DLLAPI MD5(const std::string filePath, LPBYTE hashBuffer);
-		static void SHA256(void* buffer, std::string path);
+		bool DLLAPI MD5(const BYTE * byteString, size_t byteSize, LPBYTE hashBuffer);
+		bool DLLAPI Test(void);
+		void DLLAPI SHA256(void* buffer, std::string path);
+		bool DLLAPI MD5Equal(const BYTE md5_1[MD5Size], const BYTE md5_2[MD5Size]);
+		bool DLLAPI SHA256Equal(const BYTE md5_1[MD5Size], const BYTE md5_2[MD5Size]);
 	}
 }
-
